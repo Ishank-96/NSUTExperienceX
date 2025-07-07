c62dba323a3524070a12fa54bd81e84f397ec010
Ishank <ishank282soc@gmail.com>
2025-07-07 12:57:02 +0530

    fixed update post error

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -2,7 +2,12 @@ import { Button } from 'flowbite-react';
 
 export default function CallToAction() {
   return (
-    <div className='flex border border-teal-500 p-3 justify-center items-center rounded-tl-3xl rounded-br-3xl flex-col sm:flex-row text-center'>
+    <div> </div>
+  );
+}
+
+
+  /*<div className='flex border border-teal-500 p-3 justify-center items-center rounded-tl-3xl rounded-br-3xl flex-col sm:flex-row text-center'>
       <div className='flex-1 justify-center flex flex-col'>
         <h2 className='text-2xl'>
           Want to learn HTML, CSS and JavaScript by building fun and engaging
@@ -27,6 +32,5 @@ export default function CallToAction() {
       <div className='flex-1 p-7'>
         <img src='https://media.geeksforgeeks.org/wp-content/cdn-uploads/20221114110410/Top-10-JavaScript-Project-Ideas-For-Beginners-2023.png' />
       </div>
-    </div>
-  );
-}
+    </div> */
+
