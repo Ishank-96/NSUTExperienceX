6efe11524bb41178880bbf40794129b487384606
Ishank <ishank282soc@gmail.com>
2025-07-07 13:03:28 +0530

    error fix on updatePost

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -3,6 +3,7 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { uploadImageToCloudinary } from '../cloudinary';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
